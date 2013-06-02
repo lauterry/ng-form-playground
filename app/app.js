3b0b390bf814ca10c233c56144cd50cc4f4bfa17
Thierry LAU <thierlau@gmail.com>
2013-06-03 00:58:35 +0200

    add validation

--- app/app.js
+++ app/app.js
@@ -4,8 +4,13 @@ formApp.controller('formController', function($scope){
 
     $scope.users = [];
 
+    $scope.show = true;
+
     $scope.confirm = function(user){
         $scope.users.push(user);
+        // only available from angularjs 1.1.1
+        // https://github.com/angular/angular.js/pull/1127
+        $scope.myForm.$setPristine();
         $scope.user = {};
     }
 
