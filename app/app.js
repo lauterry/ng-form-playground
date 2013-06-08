70ec70836d5c5d12ea8221d3bf2e1b67ea8933f4
Thierry LAU <thierlau@gmail.com>
2013-06-09 00:07:03 +0200

    add users resize span

--- app/app.js
+++ app/app.js
@@ -2,7 +2,29 @@ var formApp = angular.module('formApp', []);
 
 formApp.controller('formController', function($scope){
 
-    $scope.users = [];
+    $scope.users = [
+        {
+            fullname : 'Lau Thierry',
+            email : 'thierlau@gmail.com',
+            arrival : '2013-03-28',
+            night : 5,
+            total : 550.65
+        },
+        {
+            fullname : 'Letélié Sébastien',
+            email : 'sebastien.letelie@gmail.com',
+            arrival : '2013-12-23',
+            night : 3,
+            total : 345.70
+        },
+        {
+            fullname : 'Chatel Thierry',
+            email : 'tchatel@gmail.com',
+            arrival : '2013-05-14',
+            night : 1,
+            total : 45.35
+        }
+    ];
 
     $scope.confirm = function(user){
         $scope.users.push(user);
